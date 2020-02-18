ee1328abf3ddd8a252a9f42066a6e63db140fb88
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-02-17 21:23:55 -0300

    implementando novas funções

--- githubfind/src/App.js
+++ githubfind/src/App.js
@@ -1,17 +1,55 @@
+import 'dotenv/config'
+
 import React from 'react';
+import Axios from 'axios';
 
 import Navbar from './components/Navbar';
-import Search from './components/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      github: {
+        url: "http://api.github.com/users",
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        count: 7,
+        sort: "created: asc"
+      },
+      user: [],
+      repos: []
+    }
+  }
 
-      <Search />
+  getUser = (e) => {
+    const user = e.target.value;
 
-    </div>
-  );
+    const { url, client_id, client_secret, count, sort } = this.state.github;
+    Axios
+      .get(
+        `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
+      )
+      .then(({ data }) => this.setState({ user: data }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+
+        <div className="container">
+          <div className="card card-body">
+            <h1>Pesquisar Usuário</h1>
+            <p className="lead">
+              Digite o nome do usuário
+            </p>
+            <input onChange={this.getUser} type="text" id="search" className="form-control required" />
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
