032a0e4255c14092de63bba45e504c6563d29b21
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-02-19 18:34:32 -0300

    Updates

---
+++ precache-manifest.08f69709efb54e21b7cb8d09674e66b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a257f5ff253b040121177ad9505cfbd",
+    "url": "/desafio-trinity/index.html"
+  },
+  {
+    "revision": "36bfb16ab5911725b1b0",
+    "url": "/desafio-trinity/static/css/main.6a211a4e.chunk.css"
+  },
+  {
+    "revision": "f0cdc82f471191328016",
+    "url": "/desafio-trinity/static/js/2.f5c6601d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/desafio-trinity/static/js/2.f5c6601d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36bfb16ab5911725b1b0",
+    "url": "/desafio-trinity/static/js/main.d15bc416.chunk.js"
+  },
+  {
+    "revision": "3406c544610b102a515d",
+    "url": "/desafio-trinity/static/js/runtime-main.c1b627e9.js"
+  }
+]);
